425fbf99719b5b1631813610a13fabd83884f7aa
esteiner <evan.d.steiner@gmail.com>
2015-07-18 00:54:41 -0400

    error reporting toggle

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <h1>Welcome to Mutt-PHP!</h1>
 <?php
+
   include 'mutt/config.php';
+
+  //error reporting
+  if($errorReporting == 1){
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+  }
 
   //autoloading
   function autoloader($className){
     if(file_exists('mutt/core/classes/'.$className . '.php')) {
       include ('mutt/core/classes/'.$className . '.php');
     }
+
     if(file_exists('mutt/local/classes/'.$className . '.php')){
       include ('mutt/local/classes/'.$className . '.php');
     }   
