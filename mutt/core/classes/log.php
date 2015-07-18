14cdb9478be1907735d3403c2171877ffefe232a
esteiner <evan.d.steiner@gmail.com>
2015-07-18 12:37:35 -0400

    don't overwrite log file

--- mutt/core/classes/log.php
+++ mutt/core/classes/log.php
@@ -2,9 +2,15 @@
 
   class log {
     public static function write($msg) {
+      if(file_exists(LOG_DIRECTORY.LOG_FILE)){
+        $current = file_get_contents(LOG_DIRECTORY.LOG_FILE);
+      }
+
       $file = fopen(LOG_DIRECTORY.LOG_FILE,"w");
-      echo fwrite($file,$msg);
+      if(isset($current)){
+        fwrite($file,$current);
+      }
+      fwrite($file,$msg."\r\n");
       fclose($file);
-      return '';
     }
   }
