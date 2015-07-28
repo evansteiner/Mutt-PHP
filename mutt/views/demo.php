a8813173bcdf8ba2846b404f648497fbb479ed65
esteiner <evan.d.steiner@gmail.com>
2015-07-28 17:10:03 -0400

    updates to demo text

--- mutt/views/demo.php
+++ mutt/views/demo.php
@@ -7,7 +7,15 @@
   Config options include: 
     <ul>
       <li>project directory</li>
+      <li>log directory</li>
+      <li>log name</li>
       <li>error reporting toggle</li>
+      <li>inclusion of HTML header</li>
+      <li>inclusion of HTML footer</li>
+      <li>inclusion of Twitter Bootstrap</li>
+      <li>inclusion of jQuery (header)</li>
+      <li>inclusion of jQuery (footer)</li>
+      <li>inclusion of a base styles.css file</li>
     </ul>
   </p>
   
@@ -30,11 +38,11 @@
   
   <h3 class="indent-1">Local Classes</h3>
   <p class="indent-1"><?php echo localClassDemo::validate(); ?></p>
-  <p class="indent-1">Local classes are where your project specific live. These classes should go in mutt/local/classes.</p>
+  <p class="indent-1">Local classes are where your project specific classes live. These classes should go in mutt/local/classes.</p>
   
   <h2>Routing</h2>
   <p>Mutt-PHP bootstraps everything through the index.php file and then uses controllers to build pages. The basic breakdown of the URL to routes is:</p>
-  <code>{base_url}/{template}/{paramter_1}/{parameter_2}/etc...</code>
+  <code>{base_url}/{project_folder}/{template}/{paramter_1}/{parameter_2}/etc...</code>
   
   <h2>Controllers</h2>
   <p>Once a URI has been broken down, Mutt-PHP processes them through a controller to determine what page data to load. <i>All pages will require a controller to load properly.</i> Mutt-PHP comes with a <code>GenericController()</code> class which must be extended when adding new pages. To ensure proper autoloading, your custom controllers should always follow the <code>[Pagename]Controller</code> naming convention. Custom controllers should be added to mutt/controllers/local/.</p  >
@@ -51,13 +59,14 @@
       }
     }
   </pre>
+  <p>If a controller cannot be found for requested URI, Mutt-PHP will return a 404 response.</p>
   
   <h2>Views</h2>
   <p>Mutt-PHP doesn't support a specific templating engine out-of-the-box, but there's no reason you couldn't add one.</p>
-  <p>View files should be placed in mutt/views/ and then pointed to from a page controller.</p>
+  <p>View files should be placed in mutt/views/ and then be pointed to from a page controller.</p>
   
   <h2>Logging</h2>
-  <code>log::write('This is my logged message.');</code>
+  <p><code>log::write('This is my logged message.');</code></p>
   <p>Mutt-PHP has native support for basic logging. Log name and directory can be set in config.php.</p>
   
   <h2>Debugging Tools</h2>
@@ -87,4 +96,7 @@
   <h2>Troubleshooting</h2>
   <h3 class="indent-1">500 Errors</h3>
   <p class="indent-1">If you are getting a 500 error immediately after starting a new project, ensure that you're file permissions are correct. 755 is a good place to start.</p>
+
+  <h3 class="indent-1">Request Problems</h3>
+  <p class="indent-1">Since Mutt-PHP relies on bootstrapping, the .htaccess file will attempt to route all requests through index.php and look for appropriate controllers. This can be problematic if you're trying to link to a file which is in a traditional hierarchical location, such as css of js files. To account for this, you should exclude any files types that you don't want routed through a controller via the .htaccess file. The default exclusions are: js, ico, txt, gif, jpg, png, and css</p>
 </div>
