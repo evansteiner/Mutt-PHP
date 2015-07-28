69fabba3cd28bd32c47f756d7dca0ee60be9c7f6
esteiner <evan.d.steiner@gmail.com>
2015-07-28 16:28:37 -0400

    support fordefault css include and demo styles

--- public/includes/htmlHeader.php
+++ public/includes/htmlHeader.php
@@ -3,17 +3,21 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <?php
-      // if(isset($pageObject->pageTitle)){
-      //   echo "<title>$pageObject->pageTitle;</title>";
-      // }
       echo $pageObject->getTitle();
+
       if($jQueryHeader == 1) {
         include 'public/includes/jQuery.php';
       }    
       if($twitterBootstap == 1) {
         include 'public/includes/twitterBootstrapHeader.php';
       }
+      if($baseStylesFile == 1) {
+        $server = $_SERVER['SERVER_NAME'] . '/' . PROJECT_DIRECTORY;
+        echo '<link rel="stylesheet" href="http://' . $server . 'public/css/styles.css">';
+      }
     ?>
+
   </head>
   <body>
