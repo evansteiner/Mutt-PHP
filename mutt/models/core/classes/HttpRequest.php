dd7a3c41b6fd3a55fd2ef5c8a8f2a98c75b33a4c
esteiner <evan.d.steiner@gmail.com>
2015-08-06 22:30:52 -0400

    added class for http requests

---
+++ mutt/models/core/classes/HttpRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+  class HttpRequest {
+
+    function post ($url, $data) {
+      $options = array(
+          'http' => array(
+              'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+              'method'  => 'POST',
+              'content' => http_build_query($data),
+          ),
+      );
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context); 
+      return $http_response_header;  
+    }
+  }
